7cd1bdc5ec0e3fa374f47e15a986c7be33e9a36b
@hexlet/cli <support@hexlet.io>
2023-03-29 18:43:38 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 // BEGIN
-public class App {
-    public static String getForwardedVariables(String data) {
-        return Stream.of(data.split("\n"))
-                .filter(x -> x.startsWith("environment"))
-                .map(x -> x.replaceAll("environment=", ""))
-                .map(x -> x.replaceAll("\"", ""))
-                .flatMap(x -> Stream.of(x.split(",")))
-                .filter(x -> x.startsWith("X_FORWARDED_"))
-                .map(x -> x.replaceAll("X_FORWARDED_", ""))
+class App {
+    public static String getForwardedVariables(String content) {
+        return Stream.of(content.split("\n"))
+                .filter(str -> str.startsWith("environment"))
+                .map(str -> str.replaceAll("environment=", ""))
+                .map(str -> str.replaceAll("\"", ""))
+                .flatMap(str -> Stream.of(str.split(",")))
+                .filter(str -> str.startsWith("X_FORWARDED_"))
+                .map(str -> str.replaceAll("X_FORWARDED_", ""))
                 .collect(Collectors.joining(","));
     }
 }
